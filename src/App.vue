cab72e2620a43fdbf9acc2d24e52342bb493acfd
Andrew Fallows <andrew.fallows@intervarsity.org>
2018-09-12 15:55:30 -0400

    create basic quesiton component

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,30 @@
 <template>
   <div id="app">
     <HelloWorld msg="which princess are you"/>
+    <Question :question="this.question" :answers="this.answers" />
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import Question from './components/Question.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    HelloWorld,
+    Question
+  },
+  data () {
+    return {
+      question: 'What\'s Up?',
+      answers: {
+        'Answer1': 'cat1',
+        'Answer2': 'cat2',
+        'Answer3': 'cat3',
+        'Answer4': 'cat4'
+      }
+    }
   }
 }
 </script>
---
+++ src/components/Question.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="question">
+    <h1>{{ question }}</h1>
+    <ul>
+      <li v-for="(category, answer) in answers" :key="answer">
+        <a href="#" class="answer" v-on:click="selectAnswer($event)">
+          {{ answer }}
+          <span class="answer-category">{{ category }}</span>
+        </a>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Question',
+  props: {
+    question: String,
+    answers: Object
+  },
+  methods: {
+    selectAnswer (e) {
+      e.preventDefault()
+      console.log(e)
+      console.log(e.target)
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.question {
+  width: 800px;
+  height: 100px;
+}
+
+li {
+  list-style: none;
+  padding: 20px 40px;
+  margin-bottom: 15px;
+}
+
+.answer {
+  text-decoration: none;
+  padding: 20px 80px;
+  border: 1px solid #eee;
+  border-radius: 15px;
+}
+
+.answer:hover {
+  background: #eef;
+  border: 1px solid #ddf;
+}
+
+.answer-category {
+  display: none;
+}
+</style>
